65a3c5cee3098b12e1a2d9bfa687e4b14fb9ff90
Saurabh Saxena <144423649+Saurabhsaxena81@users.noreply.github.com>
2025-03-29 23:28:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,10 @@ app.use(cors({
 }));
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
-
+app.get('/",(req,res)=>{
+	res.send("hello")
+	}
+	);
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
